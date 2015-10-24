042c801c0ff730ec24514d11a9d1368ac0a631ea
Barnabas Bulpett <bbulpett@charter.net>
2015-10-24 12:41:00 -0400

    Assign game attributes, display as table in template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  @date = Date.today
 # Define the Game object
   class Game
-  	def initialize(title, link)
-  		@title = title
-  		@link = link
+  	def initialize(homeTeam, awayTeam)
+  		@homeTeam = homeTeam
+  		@awayTeam = awayTeam
   	end
-  	attr_reader :title
-  	attr_reader :link
+  	attr_reader :homeTeam
+  	attr_reader :awayTeam
   end
 
   # Pull the site
@@ -18,15 +19,13 @@ class ApplicationController < ActionController::Base
   	require 'open-uri'
   	doc = Nokogiri::HTML(open("http://www.nba.com/gameline/20151023/"))
 
-  	# fragment = Nokogiri::HTML.fragment(doc, encoding = nil)
-  	# @games = doc.xpath('//?')
-  	@games = doc.css("div.nbaModTopInfo")
-  	# @gamesArray = []
-  	# headlines.each do |h|
-  	# 	title = h.css('h2.titlebox>a').text
-  	# 	link = h.css('h2.titlebox>a')[0]['href']
-  	# 	@gamesArray << Game.new(title, link)
-  	# end
+  	games = doc.css(".nbaModTopInfo")
+  	@gamesArray = []
+  	games.each do |g|
+  		homeTeam = g.css("h5.hometeam").text
+  		awayTeam = g.css("h5.awayteam").text
+  		gamesArray = Game.new(homeTeam, awayTeam)
+  	end
 
   	render template: 'scrape_scoreboard'
   end
